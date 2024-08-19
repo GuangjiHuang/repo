8816556173abd94e4ef549ddc580893719e3e729
guangji.huang <guangji.huang@sophgo.com>
2024-08-19 11:37:40 +0800

    update, user(hgj), host(U22), date(Mon Aug 19 11:37:40 CST 2024)

--- clone_repo.sh
+++ clone_repo.sh
@@ -31,7 +31,7 @@ git_private_projects=(
 	"rtsp"
 )
 
-git_projects=("${public_projects[@]}" "${private_projects[@]}")
+git_projects=("${git_public_projects[@]}" "${git_private_projects[@]}")
 
 is_in_array()
 {
@@ -52,67 +52,96 @@ yhrepo()
 
 	# arg1 is the operation: clone, push, pull, list, go, tar
 	# arg2 is the project name
-	arg1=$1
-	local args=("$@")
+	if [[ $# -eq 0 ]]; then
+		echo "Must specify at least one argument!"
+		return
+	fi
 
-	case $arg1 in
+	local args=("$@")
+	local args_num=${#args[@]}
+
+	case ${args[0]} in
 		"clone" | "push" | "pull" | "go" | "tar")
-			let arg_num=${#args[@]}-1
-			echo "arg_num is the: ${arg_num}"
 			if [[ ${args[-1]} == "http" ]]; then
 				echo "----------using http to clone the git repo----------"
-				prefix=$http_prefix
-				select_projects=(${projects[@]}:1:${arg_num})
-			elif [[ ${args[-1]} == "ssh" ]]; then
-				echo "----------using ssh to clone the git repo----------"
-				prefix=$ssh_prefix
-				select_projects=(${projects[@]:1:${arg_num}})
+				prefix=$git_http_prefix
+				check_projects=("${git_public_projects[@]}")
 			else
 				echo "----------using ssh to clone the git repo----------"
-				prefix=$ssh_prefix
-				select_projects=(${projects[@]:1:${arg_num}})
+				prefix=$git_ssh_prefix
+				check_projects=("${git_projects[@]}")
 			fi
-
+			select_projects=(${args[@]:1:${args_num}})
 			final_projects=()
 			for select_project in ${select_projects[@]}; do
-				if is_in_array "$select_project" "$select_projects"; then
+				if is_in_array "$select_project" "${check_projects[@]}"; then
 					final_projects+=("$select_project")
 				fi
 			done
 
+			init_final_projects_num=${#final_projects[@]}
 			if [ ${#final_projects[@]} -eq 0 ]; then
-				final_projects=("${git_projects}")
+				if [[ "$prefix" == "$git_http_prefix" ]]; then
+					final_projects=("${git_public_projects[@]}")
+				else
+					final_projects=("${git_projects[@]}")
+				fi
 			fi
 
+			#echo "final projects: ${final_projects[@]}"
+
+			# ---- operate the final_projects -----
 			for project in ${final_projects[@]}; do
 				target_path=$target_dir/$project
 				project_url=$prefix/$project
 
-				if [[ $arg1 == "clone" ]]; then
-					#if [[ -d $target_path ]]; then
-					#	rm -rf $target_path
-					#fi
+				if [[ ${args[0]} == "clone" ]]; then
+					if [[ -d $target_path ]]; then
+						rm -rf $target_path
+					fi
 					#git clone $project_url $target_path >/dev/null 2>&1
+					git clone $project_url $target_path
+					if [ $? -eq 0 ]; then
+						echo "----------$project: clone success! ----------"
+					else
+						echo "----------$project: clone fail! ----------"
+					fi
+				elif [[ ${args[0]} == "push" ]]; then
 					echo "git push: $project"
-				elif [[ $arg1 == "push" ]]; then
-					echo "git push: $project"
-				elif [[ $arg1 == "pull" ]]; then
-					echo "git push: $project"
-				elif [[ $arg1 == "pull" ]]; then
-					echo "git push: $project"
-				fi
+					if [ -d $target_path ]; then
+						echo "$target_path not exists! No such project, please download first!"
+						echo "----------$project: push fail! ----------"
+					fi
+					commit_message="update, user($(whoami)), host($(hostname)), date($(date))"
+					echo $commit_message
+					cd $target_path
+					git add . && git commit -m "${commit_message}" && git push
+					cd -
+					echo "----------$project: push success! ----------"
 
-				if [ $? -eq 0 ]; then
-					echo "----------$project: clone success! ----------"
+				elif [[ ${args[0]} == "pull" ]]; then
+					echo "git pull: $project"
+				elif [[ ${args[0]} == "go" ]]; then
+					if [ $init_final_projects_num -eq 0 ]; then
+						echo "No scuh project: ${args[0]}"
+					elif [ -d $target_path ]; then
+						echo "-> $target_path"
+						cd $target_path
+					else
+						echo "$target_path not exists! No such project, please download first!"
+					fi
+					break # just run once
+				elif [[ ${args[0]} == "tar" ]]; then
+					echo "tar: $project"
 				else
-					echo "----------$project: clone fail! ----------"
+					echo "No such operation!"
 				fi
 			done
 		;;
 		"list")
 			;;
 		*)
-			echo "Not the right args: $arg1"
+			echo "Not the right args: ${args[0]}"
 			;;
 	esac
 }
