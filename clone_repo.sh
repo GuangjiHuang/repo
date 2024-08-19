ac9199bd1e4026ad2c58805340677b2336602778
guangji.huang <guangji.huang@sophgo.com>
2024-08-19 14:27:00 +0800

    update, user(hgj), host(U22), date(Mon Aug 19 14:27:00 CST 2024)

--- clone_repo.sh
+++ clone_repo.sh
@@ -47,13 +47,27 @@ is_in_array()
 	return 1
 }
 
+print_error()
+{
+	echo -e "$RED $1 $NOCOLOR"
+}
+
+print_info()
+{
+	echo -e "$GREEN $1 $NOCOLOR"
+}
+
+print_help()
+{
+	echo -e "${GREEN}$1${NOCOLOR} $2\n"
+}
+
 yh()
 {
-
 	# arg1 is the operation: clone, push, pull, list, go, tar
 	# arg2 is the project name
 	if [[ $# -eq 0 ]]; then
-		echo "Must specify at least one argument!"
+		print_error "Must specify at least one argument!"
 		return
 	fi
 
@@ -63,16 +77,14 @@ yh()
 	case ${args[0]} in
 		"clone" | "push" | "pull" | "go" | "tar")
 			if [[ ${args[-1]} == "http" ]]; then
-				echo "----------using http to clone the git repo----------"
 				local prefix=$git_http_prefix
 				local check_projects=("${git_public_projects[@]}")
 			else
-				echo "----------using ssh to clone the git repo----------"
 				local prefix=$git_ssh_prefix
 				local check_projects=("${git_projects[@]}")
 			fi
-			select_projects=(${args[@]:1:${args_num}})
-			final_projects=()
+			local select_projects=(${args[@]:1:${args_num}})
+			local final_projects=()
 			for select_project in ${select_projects[@]}; do
 				if is_in_array "$select_project" "${check_projects[@]}"; then
 					final_projects+=("$select_project")
@@ -88,6 +100,7 @@ yh()
 				fi
 			fi
 
+			let local final_projects_num=${#final_projects[@]}-1
 			#echo "final projects: ${final_projects[@]}"
 
 			# ---- operate the final_projects -----
@@ -102,45 +115,62 @@ yh()
 					#git clone $project_url $target_path >/dev/null 2>&1
 					git clone $project_url $target_path
 					if [ $? -eq 0 ]; then
-						echo "----------$project: clone success! ----------"
+						print_error "----------$project: clone success! ----------"
 					else
-						echo "----------$project: clone fail! ----------"
+						print_info "----------$project: clone fail! ----------"
 					fi
 				elif [[ ${args[0]} == "push" ]]; then
-					echo ">>>>>>>> git push: $project start ..."
+					print_info ">>>>>>>> git push: $project start ..."
 					if [ ! -d $target_path ]; then
-						echo "$target_path not exists! No such project, please download first!"
-						echo ">>>>>>>> git push: $project fail!"
+						print_error "$target_path not exists! No such project, please download first!"
+						print_error ">>>>>>>> git push: $project fail!"
 					fi
-					commit_message="update, user($(whoami)), host($(hostname)), date($(date))"
-					echo $commit_message
+					local commit_message="update, user($(whoami)), host($(hostname)), date($(date))"
 					cd $target_path
 					git add . && git commit -m "${commit_message}" && git push
 					cd -
-					echo ">>>>>>>> git push: $project success!"
+					print_info ">>>>>>>> git push: $project success!"
 
 				elif [[ ${args[0]} == "pull" ]]; then
-					echo "git pull: $project"
+					print_info "git pull: $project"
+
 				elif [[ ${args[0]} == "go" ]]; then
 					if [ $init_final_projects_num -eq 0 ]; then
-						echo "No scuh project: ${args[0]}"
+						print_error "No scuh project: ${args[0]}"
 					elif [ -d $target_path ]; then
-						echo "-> $target_path"
+						print_info "-> $target_path"
 						cd $target_path
 					else
-						echo "$target_path not exists! No such project, please download first!"
+						print_error "$target_path not exists! No such project, please download first!"
 					fi
 					break # just run once
+
 				elif [[ ${args[0]} == "tar" ]]; then
-					echo "tar: $project"
+					cd $target_dir
+					local tar_top_dir=$target_dir/mygithub_tar
+					if [ ! -d $tar_top_dir ]; then
+						mkdir -p $tar_top_dir
+					fi
+					if [ ! -d $target_dir/$project ]; then
+						print_error "$target_dir/project not exist! Can not tar"
+						continue
+					fi
+					if [ -d $tar_top_dir/$project ]; then
+						rm -rf $tar_top_dir/$project
+					fi
+					cp -r $target_dir/$project $tar_top_dir
+					if [[ $project == ${final_projects[$final_projects_num]} ]]; then
+						print_info "tar the $tar_top_dir"
+						tar -f mygithub_tar.tar -c $tar_top_dir
+					fi
 				else
-					echo "No such operation!"
+					print_error "No such operation!"
 				fi
 			done
 		;;
 		"list")
-			echo "private git repo:"
-			sign='-'
+			print_info "private git repo:"
+			local sign='-'
 			for private_repo in ${git_private_projects[@]}; do
 				if [ -d $target_dir/$private_repo ]; then
 					sign='+'
@@ -149,7 +179,7 @@ yh()
 				fi
 				echo -e "($sign) $private_repo"
 			done
-			echo "public git repo:"
+			print_info "public git repo:"
 			for public_repo in ${git_public_projects[@]}; do
 				if [ -d $target_dir/$private_repo ]; then
 					sign='+'
@@ -159,8 +189,22 @@ yh()
 				echo -e "($sign) $public_repo"
 			done
 			;;
+		"source")
+			if [ -d $target_dir/repo ]; then
+				print_info "repo: $target_dir/repo update!"
+				source $target_dir/repo/clone_repo.sh
+			else
+				print_error "repo: $target_dir/repo not exists!"
+			fi
+			;;
+		"help")
+			print_help "clone/push/pull/tar:" "shell-vim-cfg shell-script [other project] [http]/[ssh]"
+			print_help "list:" "show all the possible repo, + means the exist in your local, - means not exists!"
+			print_help "source:" "source ~/mygithub/repo/clone_*.sh, update the funcion you use(yh)"
+			print_help "help:" "show the help information"
+			;;
 		*)
-			echo "Not the right args: ${args[0]}"
+			print_error "Not the right args: ${args[0]}"
 			;;
 	esac
 }
